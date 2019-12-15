1d5f171bc97f45f05af1d3a7bb44e592fbb16dd2
transteph <transteph@protonmail.com>
2019-12-15 17:41:32 -0500

    Scraped three subcategories.

--- harvest.R
+++ harvest.R
@@ -19,8 +19,9 @@
 # install.packages('rebus')
 # install.packages('lubridate')  
 # install.packages('httr')
-#install.packages('xml2')
-install.packages('XML')
+# install.packages('xml2')
+# install.packages('XML')
+# install.packages("oai")
 
 # --------------------------------------------------
 #    --- SETTING UP ---
@@ -46,7 +47,7 @@ library(data.table)
 library(httr)
 library(xml2)
 library(XML)
-
+library(purrr)
 
 # --------------------------------------------------
 #
@@ -81,25 +82,155 @@ library(XML)
 # --------------------------------------------------
 
 
-# 
-# A.1.	Development Knowledge and Learning 
-#
+# --------------------------------------------------
+# A.1.	Development Knowledge and Learning (n=3)
+# --------------------------------------------------
+
 # retrieve document metadata from Open Knowledge Repository
-a1 <- GET("https://openknowledge.worldbank.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc&set=col_10986_31460")
-r<-content(a1, as="parsed")
-write_xml(r, "test.xml", options="format")
+r <- GET("https://openknowledge.worldbank.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc&set=col_10986_31460")
+r <- content(r, as="parsed")
+
+
+#write_xml(r, "r.xml", options="format")
 # see xml namespaces
 # xml_ns(r)
-
+abstract = xml_find_all(r, ".//dc:description") %>% xml_text() %>% str_squish()
 # find records and put metadata into data table
-dt <- data.table( 
+a1 <- data.table( 
   category = "Working Papers",
   subcategory = 1,
-  id   = xml_find_all(r, ".//dc:identifier") %>% xml_text(),
-  title = xml_find_all( r, ".//dc:title" ) %>% xml_text(),
-  abstract = xml_find_all(r, ".//dc:description") %>% xml_text(),
+  "id"   = xml_find_all(r, ".//d1:identifier") %>% xml_text(),
+  title = xml_find_all(r, ".//dc:title" ) %>% xml_text(),
+  "abstract" = abstract,
   date  = xml_find_all(r, ".//d1:datestamp") %>% xml_text(),
   stringsAsFactors = FALSE )
+
+# --------------------------------------------------
+# A.2.	ESMAP papers (n=364)
+# --------------------------------------------------
+
+#     parse url components 
+# base url for query 
+base <- "https://openknowledge.worldbank.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc&set=col_10986_"
+
+# base url with resumption token
+base0 <- "https://openknowledge.worldbank.org/oai/request?verb=ListRecords&resumptionToken=oai_dc///col_10986_"
+
+# id of subcategory
+temp_id <- "17456"
+temp_url <- paste0(base, temp_id)
+
+# url for queries past first 100 docs
+temp_url0 <- paste0(base0, temp_id)
+
+# retrieve metadata first 100 docs
+r0 <- GET(temp_url)
+r0 <-content(r0, as="parsed")
+# first 100 docs of a2 parsed.
+
+# retrieve metadata of rest of docs
+
+# r1 <- GET("https://openknowledge.worldbank.org/oai/request?verb=ListRecords&resumptionToken=oai_dc///col_10986_17456/100")
+r1 <- GET(paste0(temp_url0, "/100"))
+r1 <-content(r1, as="parsed")
+
+r2 <- GET(paste0(temp_url0, "/200"))
+r2 <-content(r2, as="parsed")
+
+r3 <- GET(paste0(temp_url0, "/300"))
+r3 <-content(r3, as="parsed")
+
+# variable rx to hold combined scrapes
+rx <- r0
+
+# combine all queries into r0
+to_add <- xml_children(r1)
+for (child in to_add) {
+  xml_add_child(rx, child)
+}
+
+to_add <- xml_children(r2)
+for (child in to_add) {
+  xml_add_child(rx, child)
+}
+
+to_add <- xml_children(r3)
+for (child in to_add) {
+  xml_add_child(rx, child)
+}
+
+# *** Not all metadata is present for each record. Using fixed loop
+# *** to make sure lengths of columns are the same
+
+# check length of id
+r_id <-  xml_find_all(rx, ".//d1:identifier") %>% xml_text()
+i <- length(r_id)
+
+
+# put metadata into data table
+category = "Working Papers"
+subcategory = 2
+id   = xml_find_all(rx, ".//d1:identifier") %>% xml_text()
+title = xml_find_all(rx, ".//dc:title" ) %>% xml_text()
+abstract = xml_find_all(rx, ".//dc:description") %>% xml_text() %>% str_squish()
+date  = xml_find_all(rx, ".//d1:datestamp") %>% xml_text()
+
+a2 <- data.table( 
+  "category" = category,
+  "subcategory" = subcategory,
+  'id' = id[1:i],
+  'title' = title[1:i],
+  'abstract'= abstract[1:i],
+  'date'  = date[1:i],
+  stringsAsFactors = FALSE )
+
+# combine a1 and a2
+combined_papers <- rbind(a1, a2, fill=TRUE)
+
+# write.xlsx(aa, "a1-a2.xlsx")
+
+
+# --------------------------------------------------
+# A.3.	Extractive Industries for Development (n=37)
+# --------------------------------------------------
+
+# id of subcategory
+temp_id <- "16297"
+temp_url <- paste0(base, temp_id)
+
+# url for queries past first 100 docs
+temp_url0 <- paste0(base0, temp_id)
+
+# retrieve metadata first 100 docs
+r0 <- GET(temp_url)
+r0 <-content(r0, as="parsed")
+
+# check length of id
+r_id <-  xml_find_all(r0, ".//d1:identifier") %>% xml_text()
+i <- length(r_id)
+
+
+# put metadata into data table
+category = "Working Papers"
+subcategory = 3
+id   = xml_find_all(r0, ".//d1:identifier") %>% xml_text()
+title = xml_find_all(r0, ".//dc:title" ) %>% xml_text()
+abstract = xml_find_all(r0, ".//dc:description") %>% xml_text() %>% str_squish()
+date  = xml_find_all(r0, ".//d1:datestamp") %>% xml_text()
+
+a3 <- data.table( 
+  "category" = category,
+  "subcategory" = subcategory,
+  'id' = id[1:i],
+  'title' = title[1:i],
+  'abstract'= abstract[1:i],
+  'date'  = date[1:i],
+  stringsAsFactors = FALSE )
+
+# combine to collection
+combined_papers <- rbind(combined_papers, a3, fill=TRUE)
+
+
 
 
 # --------------------------------------------------------
@@ -110,13 +241,13 @@ dt <- data.table(
 OUT <- createWorkbook()
 
 # Add some sheets to the workbook
-addWorksheet(OUT, "dt")
+addWorksheet(OUT, "1")
 
 # Write the data to the sheets
-writeDataTable(OUT, sheet = 1, x = dt)
+writeDataTable(OUT, sheet = 1, x = all)
 
 # Export the file
-saveWorkbook(OUT, "abstract-collection.xlsx")
+saveWorkbook(OUT, "abstract-collection1.xlsx")
 
 
 
